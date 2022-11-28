d627a32c12cef7c36de4058437a377695026c9eb
Rupali Harjai <rharjai@beesolvertechnology.com>
2022-11-28 16:57:05 +0530

    add image

--- web/src/componets/HeroSection.js
+++ web/src/componets/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src="./videos/video-3.mp4" autoPlay play loop muted/>
+        {/* <video src="./videos/video-3.mp4" autoPlay play loop muted/> */}
         <h1>Lifelong Memories</h1>
         <p>Let's start your journey with us, your dream will come true
         </p>
